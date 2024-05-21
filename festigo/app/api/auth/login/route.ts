02f821e01eb4c0c999f7282f01591382106cb88a
Niharika <goulikarniharika@gmail.com>
2024-05-21 23:42:22 +0530

    added login register routes

--- festigo/app/api/auth/login/route.ts
+++ festigo/app/api/auth/login/route.ts
@@ -3,46 +3,45 @@ import bcrypt from 'bcrypt';
 import { Role,Authentication } from "@/Utils/Enums";
 
 import { NextRequest,NextResponse } from 'next/server'
-export async function GET(){
-    return NextResponse.json({username:"Niharika"})
-}
+
+
 
 interface User {
     username: string;
-    email: string;
-    phonenumber: string;
     password: string;
-    role: string;
+   email:string;
 }
 
     
 
-export async function POST(req:NextRequest,res:NextResponse){
+export  async function POST(req:NextRequest,res:NextResponse){
     try {
         // Parse request body
         const userdata: User = await req.json();
+        const user = await client.users.findFirst({
+            where: { 
+                OR:[
+                    { username: userdata.username }, 
+                    { email: userdata.email } 
+                ]
+             }, 
+          });
+       if(user!=null){
 
-        // Generate salt for password hashing
-        const salt = await bcrypt.genSalt(11);
-
-        // Hash the password
-        const hashedPassword: string = await bcrypt.hash(userdata.password, salt);
-
-        // Create user in the database
-        const user = await client.users.create({
-            data: {
-                username: userdata.username,
-                email: userdata.email,
-                phoneNumber: userdata.phonenumber,
-                password: hashedPassword,
-                authenticationType: Authentication.EMAIL,
-                role: Role.EVENT_ORGANIZER
-            }
-        });
-   
-        // Send success response with created user data
-    //    res.st
-    //     res.json({data:userdata},{status:200});
+      
+        const hashedPassword: boolean = await bcrypt.compare(userdata.password, user.password);
+           if(hashedPassword){
+          return      NextResponse.json(user);
+           }else{
+            return    NextResponse.json({message:"incorrect password",statusbar:401})
+           }
+       }
+      
+        return NextResponse.json({"message":"user doesnot exists"})
+       
+ 
+      
+  
     } catch (error) {
         console.error("Error creating user:", error);
         // Send error response with appropriate status code
---
+++ festigo/app/api/auth/register/route.ts
@@ -0,0 +1,49 @@
+import client from "../../../db"
+import bcrypt from 'bcrypt';
+import { Role,Authentication } from "@/Utils/Enums";
+
+import { NextRequest,NextResponse } from 'next/server'
+
+
+interface User {
+    username: string;
+    email: string;
+    phonenumber: string;
+    password: string;
+    role: string;
+}
+
+    
+
+export  async function POST(req:NextRequest,res:NextResponse){
+    try {
+        // Parse request body
+        const userdata: User = await req.json();
+
+        // Generate salt for password hashing
+        const salt = await bcrypt.genSalt(11);
+
+        // Hash the password
+        const hashedPassword: string = await bcrypt.hash(userdata.password, salt);
+
+        // Create user in the database
+        const user = await client.users.create({
+            data: {
+                username: userdata.username,
+                email: userdata.email,
+                phoneNumber: userdata.phonenumber,
+                password: hashedPassword,
+                authenticationType: Authentication.EMAIL,
+                role: Role.EVENT_ORGANIZER
+            }
+        });
+   
+        // Send success response with created user data
+      
+    return NextResponse.json(user);
+    } catch (error) {
+        console.error("Error creating user:", error);
+        // Send error response with appropriate status code
+       // res.status(500).json({ error: "Error creating user" });
+    }
+}
