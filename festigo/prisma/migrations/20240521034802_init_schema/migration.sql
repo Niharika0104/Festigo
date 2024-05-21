ac455c1959412259d94af6c821ddc85e49899cf3
Niharika <goulikarniharika@gmail.com>
2024-05-21 09:24:45 +0530

    added db schema

---
+++ festigo/prisma/migrations/20240521034802_init_schema/migration.sql
@@ -0,0 +1,195 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" TEXT NOT NULL,
+    "eventName" TEXT NOT NULL,
+    "startDateTime" TIMESTAMP(3) NOT NULL,
+    "endDateTime" TIMESTAMP(3) NOT NULL,
+    "hostId" TEXT NOT NULL,
+    "venueId" TEXT NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Guest_RSVP" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+    "rsvpStatus" TEXT NOT NULL DEFAULT 'pending',
+
+    CONSTRAINT "Guest_RSVP_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "authenticationType" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Venue" (
+    "id" TEXT NOT NULL,
+    "venueName" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "capacity" INTEGER NOT NULL,
+
+    CONSTRAINT "Venue_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Vendor" (
+    "id" TEXT NOT NULL,
+    "vendorId" TEXT NOT NULL,
+    "vendorName" TEXT NOT NULL,
+    "serviceType" TEXT NOT NULL,
+    "contactInfo" TEXT NOT NULL,
+
+    CONSTRAINT "Vendor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" TEXT NOT NULL,
+    "senderId" TEXT NOT NULL,
+    "receiverId" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ChatParticipant" (
+    "id" TEXT NOT NULL,
+    "chatId" TEXT NOT NULL,
+    "participantId" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+    "participantType" TEXT NOT NULL,
+
+    CONSTRAINT "ChatParticipant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PaymentsLedger" (
+    "id" TEXT NOT NULL,
+    "paymentAmount" DECIMAL(65,30) NOT NULL,
+    "paymentDate" TIMESTAMP(3) NOT NULL,
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "payerId" TEXT NOT NULL,
+    "payeeId" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "PaymentsLedger_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Budget" (
+    "id" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+    "totalBudget" DECIMAL(65,30) NOT NULL,
+    "allocatedBudget" DECIMAL(65,30) NOT NULL,
+    "remainingBudget" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "Budget_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Channel" (
+    "id" TEXT NOT NULL,
+    "channelName" TEXT NOT NULL,
+    "channelType" TEXT NOT NULL,
+
+    CONSTRAINT "Channel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ChannelParticipant" (
+    "id" TEXT NOT NULL,
+    "channelId" TEXT NOT NULL,
+    "participantId" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+
+    CONSTRAINT "ChannelParticipant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ChannelMessage" (
+    "id" TEXT NOT NULL,
+    "channelId" TEXT NOT NULL,
+    "senderId" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ChannelMessage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Guest_RSVP_phone_key" ON "Guest_RSVP"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Guest_RSVP_phone_eventId_key" ON "Guest_RSVP"("phone", "eventId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_username_key" ON "Users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_phoneNumber_key" ON "Users"("phoneNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Vendor_vendorId_key" ON "Vendor"("vendorId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Budget_eventId_key" ON "Budget"("eventId");
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_hostId_fkey" FOREIGN KEY ("hostId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_venueId_fkey" FOREIGN KEY ("venueId") REFERENCES "Venue"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Guest_RSVP" ADD CONSTRAINT "Guest_RSVP_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChatParticipant" ADD CONSTRAINT "ChatParticipant_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChatParticipant" ADD CONSTRAINT "ChatParticipant_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChatParticipant" ADD CONSTRAINT "ChatParticipant_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PaymentsLedger" ADD CONSTRAINT "PaymentsLedger_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PaymentsLedger" ADD CONSTRAINT "PaymentsLedger_payerId_fkey" FOREIGN KEY ("payerId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PaymentsLedger" ADD CONSTRAINT "PaymentsLedger_payeeId_fkey" FOREIGN KEY ("payeeId") REFERENCES "Vendor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Budget" ADD CONSTRAINT "Budget_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChannelParticipant" ADD CONSTRAINT "ChannelParticipant_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChannelParticipant" ADD CONSTRAINT "ChannelParticipant_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChannelMessage" ADD CONSTRAINT "ChannelMessage_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
