008975c4b827aedd7dc03e24b95b520e2022276a
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-05-25 19:48:50 +0530

    auth ui almost complete

---
+++ festigo/app/auth/login/page.tsx
@@ -0,0 +1,17 @@
+import Login from "@/components/auth/Login";
+// import Designer from "../../../public/assets/designer.png"; // Correct the spelling of 'assets'
+
+import bgImage from "../../../public/assests/icons/loginLightBg.jpg";
+
+export default function LoginPage() {
+    return (
+
+        <div>
+
+            <Login />
+
+        </div>
+    );
+}
+
+
