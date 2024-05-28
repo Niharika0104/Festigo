af6a175c8d00e6eace122881717c56918514b29d
Niharika Goulikar <goulikarniharika@gmail.com>
2024-05-28 13:38:28 +0530

    branch change

---
+++ festigo/app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+
+import DashBoard from "@/components/pages/dashboard/page"
+
+export default function DashBoardComponent() {
+
+    return (
+
+
+        <div className="bg-white">
+
+            <DashBoard></DashBoard>
+
+        </div>
+    )
+}
+
+
+
+
+
+
