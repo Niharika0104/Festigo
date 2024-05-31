e5a4eea91d362cb817a279749b625fc9ffc9365e
Niharika Goulikar <goulikarniharika@gmail.com>
2024-05-31 17:48:36 +0000

    fixed default page

--- festigo/app/(dashboard)/settings/page.tsx
+++ festigo/app/(dashboard)/settings/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import ProfileInfo from '@/components/dashboard/settings/profileinfo'
 function Page() {
   return (
     <div>
+      <ProfileInfo/>
     </div>
   )
 }
