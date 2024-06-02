185253834da27ea557b768fa80e190b857baa5c3
Himanshu <himanshujain5690174@gmail.com>
2024-06-02 17:31:54 +0530

    added logout feature

--- festigo/components/common/Sidebar.tsx
+++ festigo/components/common/Sidebar.tsx
@@ -1,5 +1,4 @@
-
-"use client"
+"use client";
 
 import FestigoLogo from "@/public/assests/icons/FestigoLogo.png";
 
@@ -13,104 +12,104 @@ import { useRouter } from "next/navigation";
 
 import { useState } from "react";
 
+import Link from "next/link";
+import { Logout } from "../dashboard/Logout/Logout";
+
 export default function Sidebar() {
+  const [currentState, setCurrentState] = useState(SideBarData[0].title);
+  const [showLogout, setShowLogout] = useState(false);
 
-    const [currentState, setCurrentState] = useState(SideBarData[0].title);
+  const router = useRouter();
 
-    const router= useRouter() ;
+  return (
+    <div className=" max-w-maxContent min-h-max overflow-hidden">
+      {showLogout && <Logout setShowLogout={setShowLogout} />}
+      {/* top section  */}
+      <div className=" relative flex flex-col w-[290px] gap-4 h-[832px]">
+        <div className="relative flex items-center gap-3 p-5 shadow-md ">
+          {/* logo image   7 7 */}
 
-    return (
+          <div className="relative h-[90px] w-[90px]">
+            <Image src={Logo} alt="" className="w-full h-full bg-cover" />
+          </div>
 
+          <div className="relative max-w-maxContent flex flex-col gap-2">
+            {/* festigo image  */}
 
-        <div className=" max-w-maxContent min-h-max overflow-hidden">
-
-            {/* top section  */}
-
-            {/* w-[70%] */}
-
-            <div className=" relative flex flex-col w-[290px] gap-4 h-[832px]">
-
-                <div className="relative flex items-center gap-3 p-5 shadow-md ">
-
-                    {/* logo image   7 7 */}
-
-                    <div className="relative h-[90px] w-[90px]">
-
-                        <Image src={Logo} alt="" className="w-full h-full bg-cover" />
-
-                    </div>
-
-                    <div className="relative max-w-maxContent flex flex-col gap-2">
-
-                        {/* festigo image  */}
-
-                        <div className="realtive w-[100px] h-[30px]">
-
-                            <Image src={FestigoLogo} alt="" className="w-full h-full bg-cover" />
-
-                        </div>
-
-                        <div className="flex items-center">
-
-                            <span className="bg-yellow-300 p-1 rounded-md text-sm shadow-2xl translate-x-1">DASH</span>
-
-                            <p className="text-[#FF0000] font-bold"> BOARD </p>
-
-                        </div>
-
-
-                    </div>
-
-
-
-                </div>
-
-                {/* bottom section  */}
-
-
-                {/* Bottom section */}
-
-                <div className="flex w-full lg:w-auto flex-col gap-3 p-3 shadow-2xl pt-5">
-
-                    {SideBarData.map((data, index) => (
-
-                        <div className={`flex gap-4 items-center w-full px-3 py-2 rounded-lg cursor-pointer transition-all
-
-                          ${data.title === currentState ? "bg-[#FF0909] text-white font-semibold" : "bg-white text-gray-700 hover:bg-gray-100"}`}
-                            key={index}
-                            onClick={() => {setCurrentState(data.title);router.push(data.link??"/hello"); }}
-                        >
-                            <div className="w-6 h-6 flex justify-center items-center">
-
-                                {data.icon}
-
-                            </div>
-
-                            {data.title === "Dashboard" ? (
-
-                                <div className="flex items-center pr-7">
-
-                                    <span className="bg-yellow-300 p-1 text-black font-semibold rounded-md text-sm shadow-2xl translate-x-2">DASH</span>
-
-                                    <p className="text-[#A3AED0] font-bold ml-1 lowercase">BOARD</p>
-
-                                </div>
-                            ) : (
-
-                                <p className="">{data.title}</p>
-                            )}
-
-                        </div>
-                    ))}
-
-                </div>
-
+            <div className="realtive w-[100px] h-[30px]">
+              <Image
+                src={FestigoLogo}
+                alt=""
+                className="w-full h-full bg-cover"
+              />
             </div>
 
+            <div className="flex items-center">
+              <span className="bg-yellow-300 p-1 rounded-md text-sm shadow-2xl translate-x-1">
+                DASH
+              </span>
 
+              <p className="text-[#FF0000] font-bold"> BOARD </p>
+            </div>
+          </div>
         </div>
-    )
+
+        {/* bottom section  */}
+        <div className="flex w-full lg:w-auto flex-col gap-3 p-3 shadow-2xl pt-5">
+          {SideBarData.map((data, index) =>
+            data.title === "Log Out" ? (
+              <button
+                key={index}
+                onClick={() => {
+                  setShowLogout(true);
+                  setCurrentState(data.title);
+                }}
+                className={`flex gap-4 items-center w-full px-3 py-2 rounded-lg cursor-pointer transition-all
+                          ${
+                            data.title === currentState
+                              ? "bg-[#FF0909] text-white font-semibold"
+                              : "bg-white text-gray-700 hover:bg-gray-100"
+                          }`}
+              >
+                <div className="w-6 h-6 flex justify-center items-center">
+                  {data.icon}
+                </div>
+                <p className="">{data.title}</p>
+              </button>
+            ) : (
+              <Link
+                key={index}
+                href={data.link || "/"}
+                className={`flex gap-4 items-center w-full px-3 py-2 rounded-lg cursor-pointer transition-all
+
+                          ${
+                            data.title === currentState
+                              ? "bg-[#FF0909] text-white font-semibold"
+                              : "bg-white text-gray-700 hover:bg-gray-100"
+                          }`}
+              >
+                <div className="w-6 h-6 flex justify-center items-center">
+                  {data.icon}
+                </div>
+
+                {data.title === "Dashboard" ? (
+                  <div className="flex items-center pr-7">
+                    <span className="bg-yellow-300 p-1 text-black font-semibold rounded-md text-sm shadow-2xl translate-x-2">
+                      DASH
+                    </span>
+
+                    <p className="text-[#A3AED0] font-bold ml-1 lowercase">
+                      BOARD
+                    </p>
+                  </div>
+                ) : (
+                  <p className="">{data.title}</p>
+                )}
+              </Link>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
-
--- festigo/components/common/Topbar.tsx
+++ festigo/components/common/Topbar.tsx
@@ -1,5 +1,3 @@
-
-
 import InputField from "./InputField";
 
 import { PiUserCircleDuotone } from "react-icons/pi";
@@ -9,61 +7,40 @@ import { useState } from "react";
 import { CiSearch } from "react-icons/ci";
 
 export default function TopBar() {
+  const [textData, setTextData] = useState({ text: "" });
 
+  function changeHandler(event: any) {
+    setTextData({ text: event.target.value });
+  }
 
-    const [textData, setTextData] = useState({ text: "" });
+  return (
+    <div>
+      <div className="relative w-full flex justify-between items-baseline mt-3 px-3 pr-6">
+        <div className="flex flex-col pl-8">
+          <p className="text-[#707EAE]">Hii Radhika, </p>
 
-    function changeHandler(event: any) {
-
-        setTextData({ text: event.target.value });
-
-    }
-
-
-    return (
-
-        <div>
-
-            <div className="relative w-full flex justify-between items-baseline mt-3 px-3 pr-6">
-
-                <div className="flex flex-col pl-8">
-
-                    <p className="text-[#707EAE]">Hii Radhika, </p>
-
-                    <h1 className="text-[#2B3674] text-3xl font-bold">
-
-                        Welcome to Festigo!
-
-                    </h1>
-
-                </div>
-
-                <div className="flex gap-6 justify-center items-center">
-
-                    <div className="w-[350px]">
-                        <InputField
-                            type="text"
-                            placeholder="Enter Text to Search"
-                            value={textData.text}
-                            name="text"
-                            onChange={changeHandler}
-                            required={false}
-                            icon={<CiSearch />}
-                        ></InputField>
-                    </div>
-                    <div>
-                        <PiUserCircleDuotone size={50} />
-                    </div>
-                </div>
-            </div>
-
-
+          <h1 className="text-[#2B3674] text-3xl font-bold">
+            Welcome to Festigo!
+          </h1>
         </div>
-    )
 
+        <div className="flex gap-6 justify-center items-center">
+          <div className="w-[350px] z-[0]">
+            <InputField
+              type="text"
+              placeholder="Enter Text to Search"
+              value={textData.text}
+              name="text"
+              onChange={changeHandler}
+              required={false}
+              icon={<CiSearch />}
+            ></InputField>
+          </div>
+          <div>
+            <PiUserCircleDuotone size={50} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
-
-
-
